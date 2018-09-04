94000cc676db1015fba361170467ad66c5d8ee5f
lulldev <lulldev@yandex.ru>
2018-09-04 23:59:51 +0300

    add config, create spaces folder

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const commandLineArgs = require('command-line-args');
 const fs = require('fs');
 const _  = require('lodash');
-const os = require('os');
 
 
 const startupOptionDifinition = [
-  { name: 'init', alias: 'i', type: Boolean },
-  { name: 'create-space', alias: 'c', type: String },
+  { name: 'multiclone', alias: 'm', type: Boolean },
 ];
 
 const startupOptions = commandLineArgs(startupOptionDifinition);
-console.log(startupOptions);
+const SPACES_PATH = `${process.cwd()}/spaces`;
 
-if (!_.isNull(startupOptions.init)) {
-  console.log('INIT!', os.homedir());
+if (!fs.existsSync(SPACES_PATH)){
+  fs.mkdirSync(SPACES_PATH);
+}
+
+if (_.hasIn(startupOptions, 'multiclone')) {
+  // TODO: get space.json & parse -> clone all from config in ./space
 }
