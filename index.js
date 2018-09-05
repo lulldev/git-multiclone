cb430b6d7ea13dc7e32dbe6b4e42650bb0071250
lulldev <lulldev@yandex.ru>
2018-09-05 11:51:18 +0300

    add init script, create check space file

--- index.js
+++ index.js
@@ -4,16 +4,31 @@ const _  = require('lodash');
 
 
 const startupOptionDifinition = [
-  { name: 'multiclone', alias: 'm', type: Boolean },
+  { name: 'init_spaces', alias: 'i', type: Boolean },
+  { name: 'multiclone_space', alias: 'm', type: String },
 ];
 
 const startupOptions = commandLineArgs(startupOptionDifinition);
 const SPACES_PATH = `${process.cwd()}/spaces`;
 
-if (!fs.existsSync(SPACES_PATH)){
-  fs.mkdirSync(SPACES_PATH);
-}
 
-if (_.hasIn(startupOptions, 'multiclone')) {
-  // TODO: get space.json & parse -> clone all from config in ./space
+if (_.hasIn(startupOptions, 'multiclone_space')) {
+  const spacePath = `./${startupOptions.multiclone_space}`;
+  if (!fs.existsSync(spacePath)) {
+    console.log('Space file not found!');
+    process.exit(-1);
+  }
+}
+else if (_.hasIn(startupOptions, 'init_spaces')) {
+  if (!fs.existsSync(SPACES_PATH)){
+    fs.mkdirSync(SPACES_PATH);
+    fs.writeFileSync(`${SPACES_PATH}/example.space.json`, JSON.stringify({
+      "space-name": [
+        {
+          "repo1": "https://github.com/",
+          "repo2": "https://github.com/"
+        }
+      ]
+    }));
+  }
 }
